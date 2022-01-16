61f51075c8b54733af2c032466c0dc190c0b3d7f
zhouxu <zhouxu0928@126.com>
2022-01-16 22:54:30 +0800

    feat:first commit

---
+++ ac-009/ac-009.go
@@ -0,0 +1,46 @@
+package main
+
+import "container/list"
+
+func main() {
+
+}
+
+type CQueue struct {
+	stack1, stack2 *list.List
+}
+
+func Constructor() CQueue {
+	return CQueue{
+		stack1: list.New(),
+		stack2: list.New(),
+	}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.stack1.PushBack(value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	//如果第2个栈为空
+	if this.stack2.Len() == 0 {
+		for this.stack1.Len() > 0 {
+			tempValue := this.stack1.Back()
+			removeValue := this.stack1.Remove(tempValue)
+			this.stack2.PushBack(removeValue)
+		}
+	}
+	if this.stack2.Len() != 0 {
+		e := this.stack2.Back()
+		this.stack2.Remove(e)
+		return e.Value.(int)
+	}
+	return -1
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AppendTail(value);
+ * param_2 := obj.DeleteHead();
+ */
